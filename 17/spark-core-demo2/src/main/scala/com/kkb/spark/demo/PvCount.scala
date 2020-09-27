bfb68a676372b74644dbf092f2fd4399d7478be8
yibeiou <yibei.ou@gmail.com>
2020-09-27 12:35:41 +1000

    PvCount, getPath
    PvCount, getPath

--- 17/spark-core-demo2/src/main/scala/com/kkb/spark/demo/PvCount.scala
+++ 17/spark-core-demo2/src/main/scala/com/kkb/spark/demo/PvCount.scala
@@ -11,7 +11,7 @@ object PvCount {
     val sc = new SparkContext(sparkConf)
     sc.setLogLevel("warn")
     //3、读取数据文件
-    val dataRDD: RDD[String] = sc.textFile("/Users/yibeiou/Downloads/IT/Big_Data_Developer/17/第十七章 spark计算框架/第2节 sparkCore第二次课/SparkCore第二次课/工程代码/spark-core-demo2/src/main/resources/access.log")
+    val dataRDD: RDD[String] = sc.textFile(this.getClass().getClassLoader.getResource("access.log").getPath)
     //4、统计PV
     val pv: Long = dataRDD.count()
     println(s"pv:$pv")
